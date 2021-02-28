cfb393d65fa6175284ffdd4be9c510b8701c4fca
hjhai_home <sky4160864@163.com>
2021-02-28 17:03:37 +0800

    init

--- src/main/java/com/cap/dao/RoleDao.java
+++ src/main/java/com/cap/dao/RoleDao.java
@@ -1,10 +1,24 @@
 package com.cap.dao;
 
 import com.cap.entity.Role;
+import org.apache.ibatis.annotations.Param;
 
 public interface RoleDao {
-    public Role getRole(Long id);
+    /**
+     * 获取角色
+     * @param id 主键
+     * @return /
+     */
+    public Role getRole(@Param("id") Long id);
+
+    /**
+     * 获取角色
+     * @param roleName 角色名称
+     * @return /
+     */
     public Role findRole(String roleName);
+
     public int deleteRole(Long id);
+
     public int insertRole(Role role);
 }
